88e23e61bd80c741adb594883b375004d2715efd
Paul Seehofer <seehofer.paul@gmail.com>
2018-04-16 18:38:26 +0200

    initial commit

---
+++ src/main/java/com/valdron/studienprojekt/StudienprojektApplication.java
@@ -0,0 +1,20 @@
+package com.valdron.studienprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class StudienprojektApplication {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World!";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudienprojektApplication.class, args);
+	}
+}
