65c38a900d20f2abe98d0c13ecaa4e260bb11e09
Paul Seehofer <seehofer.paul@gmail.com>
2018-05-26 12:11:31 +0200

    added rest endpoints to gateway(still empty)

--- gateway/src/main/java/com/valdron/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/valdron/gateway/GatewayApplication.java
@@ -2,11 +2,32 @@ package com.valdron.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class GatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+	@RequestMapping(path = "formUpload", method = RequestMethod.POST, consumes = "multipart/form-data")
+	public void multipartFormUpload(/*TODO*/) {
+		// TODO: Implement Method
+	}
+
+	@RequestMapping(path = "pictures", method = RequestMethod.POST, consumes = "image/*")
+	public void pictureUpload(/*TODO*/) {
+		// TODO: Implement Method
+	}
+
+	@RequestMapping(path = "picturedata", method = RequestMethod.POST, consumes = "application/json")
+	// This method requires that the picture already exists.
+	public void pictureDataUpload(/*TODO*/) {
+		// TODO: Implement Method
+
 	}
 }
